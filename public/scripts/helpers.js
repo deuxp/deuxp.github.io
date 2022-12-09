40894e4d2e40da7886f600410a44f88c98d692b5
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-12-09 12:43:26 -0500

    wip

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,3 +1,5 @@
 export const setStyles = (elem, style) => {
   Object.assign(elem.style, style);
 };
+
+export const testModule = _ => console.log("module is connected!");
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,15 +1,7 @@
-import { setStyles } from "./helpers.js";
+import { setStyles, testModule } from "./helpers.js";
 
 window.onload = () => {
   const name = document.querySelector(".name");
   const title = document.querySelector(".job-title");
-
-  let red = false;
-
-  name.addEventListener("mouseout", _ => {
-    setStyles(name, { color: "black" });
-  });
-  name.addEventListener("mouseover", _ => {
-    setStyles(name, { color: "red" });
-  });
+  testModule();
 };
